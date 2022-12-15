c01e46e266540bb3b1ac1cc8451ddb3cb3a0895b
RAMATT89 <matdesign20@gmail.com>
2022-12-15 13:56:22 -0800

    changes #2

--- public/index.js
+++ public/index.js
@@ -1,12 +1,10 @@
+"use strict";
 // var sport = 'football';
 // var id = 5;
-
 let name = 'Danny';
 name.toLowerCase();
 console.log(name); // Danny - the string method didn't mutate the string
-
 let arr = [1, 3, 5, 7];
 arr.pop();
 console.log(arr); // [1, 3, 5] - the array method mutated the array
-
-name = 'Anna' // Assignment gives the primitive a new (not a mutated) value
+name = 'Anna'; // Assignment gives the primitive a new (not a mutated) value
